207480570e4a365eefcf25d68c9cff71675cdc72
TREVAND174 <trevoranderson17@gmail.com>
2014-04-13 06:15:30 -0700

    HOLY COMMIT BATMAN

--- BackEnd/httpstuff.h
+++ BackEnd/httpstuff.h
@@ -6,7 +6,58 @@
 #include "cpprest/producerconsumerstream.h"
 #include <iostream>
 #include <sstream>
-pplx::task<int> postTest();
+#include <vector>
+#include <locale>         // std::locale, std::ctype, std::use_facet
+#include <string>         // std::wstring
+const std::basic_string<wchar_t> url = L"http://crud.meteor.com/collectionapi/";
+#define STARTB 0x43
+#define SELECTB 0x51
+#define UPB 0x57
+#define DOWNB 0x53
+#define LEFTB 0x41
+#define RIGHTB 0x44
+#define AB 0x59
+#define BB 0x5A
+// Scan Codes
+#define STARTK 0x2E
+#define SELECTK 0x10
+#define UPK 0x11
+#define DOWNK 0x1F
+#define LEFTK 0x1E
+#define RIGHTK 0x20
+#define AK 0x2D
+#define BK 0x2C
+pplx::task<int> postTest(int color, int row, int col);
 int testPost();
 int bingtest();
 int getTest();
+pplx::task<int> putTest(int color, std::basic_string<wchar_t> id);
+extern std::string jsonval;
+struct inputDB
+{
+	inputDB(int m = -1,int mk = -1, long long t = -1)
+	{
+		move = m;
+		movek = mk;
+		timestamp = t;
+	}
+	int move;
+	int movek;
+	long long timestamp;
+};
+struct screenVal
+{
+	screenVal(std::basic_string<wchar_t> sid = L"", int r = -1, int c = -1, int clr = -1)
+	{
+		id = sid;
+		row = r;
+		col = c;
+		color = clr;
+	}
+	std::basic_string<wchar_t> id;
+	int row;
+	int col;
+	int color;
+};
+ std::vector<inputDB> parseInputDB();
+ std::vector<screenVal> parseScreenVals();
--- BackEnd/main.cpp
+++ BackEnd/main.cpp
@@ -3,17 +3,126 @@
 #include "GameBoyScreen.h"
 #include "MouseMovement.h"
 #include "httpstuff.h"
+#include <vector>
+#include <algorithm>
+
+
 using namespace std;
+bool operator <(inputDB A, inputDB B)
+{
+	return (A.timestamp < B.timestamp);
+}
+bool operator <(screenVal A, screenVal B)
+{
+	if (A.row < B.row)
+	{
+		return true;
+	}
+	if (B.row < A.row)
+	{
+		return false;
+	}
+	if (A.col < B.col)
+	{
+		return true;
+	}
+	return false;
+}
+bool operator ==(screenVal A, screenVal B)
+{
+	return A.row == B.row && A.col == B.col;
+}
+void applyMove(inputDB k)
+{
+	MouseMovement cur;
+	cur.KeyEvent(k.move, k.movek);
+	Sleep(500);
+}
+// Looks up the id in the vector
+std::basic_string<wchar_t>findID(int row, int col, std::vector<screenVal> haystack)
+{
+	for (auto k : haystack)
+	{
+		if (k.row == row && k.col == col)
+		{
+			return k.id.substr(1, k.id.size() - 2); // cut off escape chars
+		}
+	}
+	return L"";
+}
+
+void updateDifference(GameBoyScreen & fresh, GameBoyScreen & stale, std::vector<screenVal> toSearch)
+{
+	for (int y = 0; y < GBHEIGHT; y += scalefactor)
+	{
+		for (int x = 0; x < GBWIDTH; x += scalefactor)
+		{
+
+			if (fresh.pixels[y][x] != stale.pixels[y][x])
+			{
+				//update according to fresh's color
+				auto id = findID(y / scalefactor, x / scalefactor, toSearch);
+				if (id == L"")
+				{
+					// TODO Post the new ones to the database
+				}
+				else
+				{
+					putTest(fresh.pixels[y][x], id).wait(); // .wait() ???
+				}
+			}
+		}
+	}
+
+}
+// this function never exits
+void client()
+{
+	// Grab a copy of the screen vals so we can have ids to update
+	auto screenvals = parseScreenVals();
+
+	long long lastTS = 0;
+	auto screen = GameBoyScreen();
+	while (true)
+	{
+		// TODO Get list of new moves and act on most recent ones
+		getTest();
+		vector<inputDB> toMove = parseInputDB();
+		sort(toMove.begin(), toMove.end());
+		// for each move, if its timestamp isn't too old, run the move
+		for (auto & k : toMove)
+		{
+			if (k.timestamp > lastTS)
+			{
+				applyMove(k);
+				lastTS = k.timestamp;
+			}
+		}
+
+		auto newscreen = GameBoyScreen(); // reset gameboyscreen
+		if (newscreen != screen)
+		{
+			updateDifference(newscreen, screen, screenvals);
+			screen = GameBoyScreen();
+		}
+	}
+}
+
 int main()
 {
+	//auto screenvals = parseScreenVals();
+	//auto id = findID(0, 0, screenvals);
+	//putTest(2, id);
 
-	auto f = getTest();
+	client();
+	auto s = parseScreenVals();
 	cout << endl << "Posting" << endl;
-	auto p = testPost();
+	auto p = postTest(1, 0, 0);
 	cout << "Done posting" << endl << endl;
-	f = getTest();
 
+	getTest();
 	//auto teemo = new GameBoyScreen();
+	parseInputDB();
 	cout << "hello world" << endl;
 	//test();
 	return 0;
