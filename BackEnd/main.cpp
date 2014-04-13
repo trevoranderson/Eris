63c8ff865b5b817ed012f3c3c8f0b85bee533fb7
TREVAND174 <trevoranderson17@gmail.com>
2014-04-12 19:53:54 -0700

    Some HTTP request stuff (TODO, FIX THE 401 IN POST)

--- BackEnd/main.cpp
+++ BackEnd/main.cpp
@@ -2,9 +2,19 @@
 #include "ImageProc.h"
 #include "GameBoyScreen.h"
 #include "MouseMovement.h"
+#include "httpstuff.h"
 using namespace std;
 int main()
 {
-	auto teemo = new GameBoyScreen();
+
+	auto f = getTest();
+	cout << endl << "Posting" << endl;
+	auto p = testPost();
+	cout << "Done posting" << endl << endl;
+	f = getTest();
+
+	//auto teemo = new GameBoyScreen();
 	cout << "hello world" << endl;
+	//test();
+	return 0;
 }
